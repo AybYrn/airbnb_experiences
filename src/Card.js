8b01cce04ce458994b2b545edf5dd88bd7335809
AybYrn <aybukeyaren--@outlook.com>
2022-08-02 13:24:27 +0300

    Airbnb Card part is still progress

--- src/Card.js
+++ src/Card.js
@@ -6,28 +6,14 @@ export default function Cart() {
   return (
     <div className="card">
       <img src={Katie} alt="Katie-zafere" className="cardImg"></img>
-      <div className="rates">
-        <div>
-          <img src={Star} alt="Star" className="cardStar"></img>
-        </div>
-        <div className="country">
-          (6)
-          <svg
-            width="2"
-            height="2"
-            viewBox="0 0 2 2"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="1" cy="1" r="1" fill="#918E9B" />
-          </svg>
-          USA
-        </div>
+      <div className="card_stats">
+        <img src={Star} alt="Star" className="cardStar"></img>
+        <span>5.0</span>
+        <span className="gray">(6) • </span>
+        <span className="gray">USA</span>
       </div>
       <p>Life lessons with Katie Zaferes</p>
-      <p>
-        <span>From $136</span> / person
-      </p>
+      <p><span className="bold">From $136 </span>/ person</p>
     </div>
   );
 }
